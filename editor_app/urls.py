0625c9bc15025510b156e3f57bebdb2859eb75ec
CFC-Tech-AI <pythoncfc@gmail.com>
2023-12-22 18:12:22 +0530

    commit

--- editor_app/urls.py
+++ editor_app/urls.py
@@ -3,5 +3,5 @@ from .views import process_image, download_image
 
 urlpatterns = [
     path('', process_image, name='process_image'),
-    path('download/', download_image, name='download_image'),
+    
 ]
